f002a10d3586a222abb672a377ae1d5206de9811
Junior Gantin <nioperas06@gmail.com>
2025-02-13 14:21:51 +0100

    Change cloudinary

--- lib/Vue3Signature.vue
+++ lib/Vue3Signature.vue
@@ -180,7 +180,7 @@ onMounted(() => {
   const context = canvas.getContext("2d");
   const img = new Image();
   img.crossOrigin = "anonymous";
-  img.src = "https://res.cloudinary.com/nioperas06/image/upload/v1686640423/rsz_1frame_1_tix3fp.png";
+  img.src = "https://cultured-experience.surge.sh/Schema.png";
   img.onload = () => {
     context.drawImage(img, 0, 0, 200, 400);
   }
